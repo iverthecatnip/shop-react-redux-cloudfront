6c5c5ae03f73c404e213bf7db042ce291871f4a3
atolstova <atolstova@edmunds.com>
2023-05-24 15:28:16 +0600

    task 8

--- src/components/pages/PageOrders/components/Orders.tsx
+++ src/components/pages/PageOrders/components/Orders.tsx
@@ -12,9 +12,10 @@ import {
   useInvalidateOrders,
   useOrders,
 } from "~/queries/orders";
+import { Typography } from "@mui/material";
 
 export default function Orders() {
-  const { data } = useOrders();
+  const { data, isFetching } = useOrders();
   const invalidateOrders = useInvalidateOrders();
   const { mutate: deleteOrder } = useDeleteOrder();
 
@@ -30,39 +31,44 @@ export default function Orders() {
             <TableCell align="right">Action</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {data?.map((order) => (
-            <TableRow key={order.id}>
-              <TableCell component="th" scope="row">
-                {order.address?.firstName} {order.address?.lastName}
-              </TableCell>
-              <TableCell align="right">{order.items.length}</TableCell>
-              <TableCell align="right">{order.address?.address}</TableCell>
-              <TableCell align="right">
-                {order.statusHistory[order.statusHistory.length - 1].status}
-              </TableCell>
-              <TableCell align="right">
-                <Button
-                  size="small"
-                  color="primary"
-                  component={Link}
-                  to={order.id}
-                >
-                  Manage
-                </Button>
-                <Button
-                  size="small"
-                  color="secondary"
-                  onClick={() =>
-                    deleteOrder(order.id, { onSuccess: invalidateOrders })
-                  }
-                >
-                  Delete
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        {
+          isFetching && <Typography>Loading ... </Typography>
+        }
+        { !isFetching &&
+          <TableBody>
+            {data?.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell component="th" scope="row">
+                  {order.delivery?.firstName} {order.delivery?.lastName}
+                </TableCell>
+                <TableCell align="right">{/*{order.items.length}*/}</TableCell>
+                <TableCell align="right">{order.delivery?.address}</TableCell>
+                <TableCell align="right">{order.status}</TableCell>
+                <TableCell align="right">
+                  <Button
+                    size="small"
+                    color="primary"
+                    component={Link}
+                    to={order.id}
+                  >
+                    Manage
+                  </Button>
+                  <Button
+                    size="small"
+                    color="secondary"
+                    onClick={() =>
+                      deleteOrder(order.id, { onSuccess: invalidateOrders })
+                    }
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+        }
+
       </Table>
     </TableContainer>
   );
